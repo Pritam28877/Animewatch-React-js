c79bce1cccfcd91b2c3cfce3bb81471c8c6996f8
Pritam Das <dpritam28577@gmail.com>
2023-08-25 02:08:05 +0530

    add the header with styleing to the header Of the home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Popular from "./components/Popular";
 import AnimeDetails from "./components/AnimeDetails";
 import "./App.css";
 import { useGlobalContext } from "./context/apiContext";
+import Homepage from "./components/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Popular />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/anime/:id" element={<AnimeDetails />} />
       </Routes>
     </BrowserRouter>
